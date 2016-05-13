af3ba37d45c6775bc402689532d58b7913760f01
jtam11 <justinktam@gmail.com>
2016-05-13 10:44:10 -0700

    Added index for tokens

--- db/migrate/20160513032446_devise_create_admins.rb
+++ db/migrate/20160513032446_devise_create_admins.rb
@@ -36,7 +36,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration
 
     add_index :admins, :email,                unique: true
     add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :admins, :confirmation_token,   unique: true
+    add_index :admins, :unlock_token,         unique: true
   end
 end
