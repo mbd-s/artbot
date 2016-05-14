508fee1216f29f9ca080c688423a7beeb335555d
jtam11 <justinktam@gmail.com>
2016-05-14 16:34:08 -0700

    Added ext_show link to art model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514232302) do
+ActiveRecord::Schema.define(version: 20160514233310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160514232302) do
     t.datetime "updated_at",    null: false
     t.string   "object_number"
     t.integer  "artist_id"
+    t.string   "ext_show"
   end
 
   add_index "arts", ["artist_id"], name: "index_arts_on_artist_id", using: :btree
