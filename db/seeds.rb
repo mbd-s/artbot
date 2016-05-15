6c38852b09f8541f632c15a32536926d71162b59
morgansharif <morgan.sharif@gmail.com>
2016-05-14 23:15:42 -0700

    update db seed for 2 models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,42 @@
+#sample query strings to build db
 
-require 'http'
-require 'json'
-
-params = {:key => "#{ENV['RIJKMUSEUM_API_TOKEN']}", :format => 'json', :maker => 'Vincent van Gogh', :type => 'painting.png', :imgonly => 'True'}
-base_endpoint = "https://www.rijksmuseum.nl/api/en/collection"
-
-rc = HTTP.get(base_endpoint, params: params)
-puts rc
-a = JSON.parse(rc.body)
-puts a
-
-#query strings to build db
-
-
-# '&format=json&maker=Johannes+Vermeer&type=painting&imgonly=True'
+# 'https://www.rijksmuseum.nl/api/en/collection?' + ENV['RIJKMUSEUM_API_TOKEN'] + '&format=json&maker=Johannes+Vermeer&type=painting&imgonly=True'
 # 'https://www.rijksmuseum.nl/api/en/collection?' + ENV['RIJKMUSEUM_API_TOKEN'] + '&format=json&maker=Vincent+van+Gogh&type=painting&imgonly=True'
 # 'https://www.rijksmuseum.nl/api/en/collection?' + ENV['RIJKMUSEUM_API_TOKEN'] + '&format=json&maker=Rembrandt+Harmensz.+van+Rijn&type=painting&imgonly=True'
-
-
-art_list = [
-  {:title => 'Title1', :artist => 'Artist1', :image => 'img1.png',  :year => '1662',  :century => 'Sixteenth',  :medium => 'Mixed media'},
-  {:title => 'Title2', :artist => 'Artist2', :image => 'img2.png',  :year => '1777',  :century => 'Sixteenth',  :medium => 'Some media'},
-  {:title => 'Title3', :artist => 'Artist3', :image => 'img3.png',:year => '1888', :century => 'Eighteenth', :medium => 'Other media'}
-  ]
-
-
-# ----------MAIN---------- #
+# single object by objectNumber 'https://www.rijksmuseum.nl/api/en/collection/SK-C-251?' + 'key=ENV['RIJKMUSEUM_API_TOKEN']' + '&format=json'
 Art.destroy_all
+p "All old art has been cleared."
 
-art_list.each do |art|
-  Art.create(art)
-  p "made an art!"
+# art lists
+vermeer_art = [
+  {title: 'Woman Reading a Letter', image: 'http://lh3.ggpht.com/_hNgP8xlzkSVD_XfBIy3j6BSWyGqdc0N921xFJbXG7jwFbQ7hi8IuTO6AIAsQf_RI3_dt4_EEncuuLY5pVO0vT50qpY=s0', object_number: 'SK-C-251', year: '1663', century: '17th', medium: 'painting', ext_show: ''},
+  {title: 'The Milkmaid', image: 'http://lh3.ggpht.com/XUw3NdeiA0MsT-mygp8NT1oPUd17GB3BK2nSRYvyTAY-N64KwsLOO5QU3muSDEk2AdMzonIVLgjge581mOobVqsQrA=s0', object_number: 'SK-A-2344', year: '1660', century: '17th', medium: 'painting', ext_show: ''},
+  {title: 'The Love Letter', image: 'http://lh4.ggpht.com/cIUsjrkQbRd5BMh9nv7nxib8lKisKC0CEmUdJTFvRkcqaT65EyLwQH8fBnBSY8tAnprq4VzGfIQ1sVbp6h0vAWWf4vEW=s0', object_number: 'SK-A-1595', year: '1669', century: '17th', medium: 'painting', ext_show: ''}
+]
+van_gogh_art = [
+  {title: 'Self-portrait', image: 'http://lh4.ggpht.com/RKAJ3z2mOcw83Ju0a7NIp71oUoJbVWJQzxwki5PSERissvWIrELCuxxGZ12U0PeAnf6WLkRCzpFdvjweUBjlcr2I4dl_=s0', object_number: 'SK-A-3262', year: '1887', century: '19th', medium: 'painting', ext_show: ''}
+]
+rembrandt_art = [
+  {title: "Militia Company of District II under the Command of Captain Frans Banninck Cocq, Known as the 'Night Watch'", image: 'http://lh6.ggpht.com/ZYWwML8mVFonXzbmg2rQBulNuCSr3rAaf5ppNcUc2Id8qXqudDL1NSYxaqjEXyDLSbeNFzOHRu0H7rbIws0Js4d7s_M=s0', object_number: 'SK-C-5', year: '1642', century: '17th', medium: 'painting', ext_show: ''}
+]
+
+# artists
+vermeer = {name: "Johannes Vermeer", birth: "1632", death: "1675", nationality: "Dutch"}
+van_gogh = {name: "Vincent van Gogh", birth: "1853", death: "1890", nationality: "Dutch"}
+rembrandt = {name: "Rembrandt Harmensz. van Rijn", birth: "1606", death: "1669", nationality: "Dutch"}
+
+# art/artist creator/associator
+def add_art(artist, art_list)
+  a = Artist.create(artist)
+  art_list.each do |art|
+    painting = Art.create(art)
+    a.arts << painting
+  end
 end
+
+# -------MAIN------- #
+
+add_art(vermeer, vermeer_art)
+add_art(van_gogh, van_gogh_art)
+add_art(rembrandt, rembrandt_art)
+# p "Created #{Artist.count} artists, and #{Art.count} paintings."
