bd9baceab5a2e47704384419d1a4a8ade70c5c43
morgansharif <morgan.sharif@gmail.com>
2016-05-16 00:53:11 -0700

    prevent slack_bot from initializing during rake commands

--- config/initializers/slack_bot.rb
+++ config/initializers/slack_bot.rb
@@ -1,3 +1,5 @@
-require_relative "./../../bot/artbot.rb"
-
-SlackBotEM.start( ENV['SLACK_API_TOKEN'] )
+unless ( File.basename($0) == 'rake')
+   # exclude artbot from initializing during 'rake' commands
+   require_relative "./../../bot/artbot.rb"
+   SlackBotEM.start( ENV['SLACK_API_TOKEN'] )
+end
