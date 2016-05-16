410f9bef14c5de0fb7a430a188005e3f4196e02b
mbd-s <michaelbsilverberg@gmail.com>
2016-05-16 16:44:27 -0700

    new question form, not yet saving data

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,21 @@
 class QuestionsController < ApplicationController
+  before_action :set_art
 
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to art_path(@art)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,6 +25,20 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_art
+    @art = Art.find(params[:art_id])
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def question_params
+    params.require(:question).allow(:text, :answer, :art_id)
   end
 
 end
