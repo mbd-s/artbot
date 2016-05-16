00825c2a2356fa5d4b01b06fe0dd24ec206a36ab
cloverharvest <anna@schwabdev.com>
2016-05-16 15:04:03 -0700

    Adds nested route for art and questions via resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :arts
+  resources :arts do
+    resources :questions, except: [:show]
+  end
 end
