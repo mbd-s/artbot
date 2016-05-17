6a644493dc0c40417ab88152d30e0e80bb220402
jtam11 <justinktam@gmail.com>
2016-05-17 15:16:07 -0700

    Added artist create feature

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,27 @@
 class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to arts_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :birth, :death, :nationality)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
 end
