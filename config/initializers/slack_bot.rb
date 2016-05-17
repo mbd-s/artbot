ab72b70c599bbb2d84fea91a45cb3fd10de0bf9a
cloverharvest <anna@schwabdev.com>
2016-05-17 13:43:14 -0700

    Creates artists controller

--- config/initializers/slack_bot.rb
+++ config/initializers/slack_bot.rb
@@ -1,5 +1,5 @@
-unless ( File.basename($0) == 'rake')
-   # exclude artbot from initializing during 'rake' commands
-   require_relative "./../../bot/artbot.rb"
-   SlackBotEM.start( ENV['SLACK_API_TOKEN'] )
-end
+# unless ( File.basename($0) == 'rake')
+#    # exclude artbot from initializing during 'rake' commands
+#    require_relative "./../../bot/artbot.rb"
+#    SlackBotEM.start( ENV['SLACK_API_TOKEN'] )
+# end
