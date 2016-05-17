2997693b1d5b7812ea78d481b4a4b2d4da58e184
cloverharvest <anna@schwabdev.com>
2016-05-17 13:50:24 -0700

    Creates boilerplate methods for artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,30 @@
 class ArtistsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
