94fd0c2c121c67082ef5b9a99a1e5e1c43ed78c8
jtam11 <justinktam@gmail.com>
2016-05-18 12:14:30 -0700

    Added some admin validation testing

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -19,4 +19,6 @@ RSpec.describe Admin, type: :model do
 
  describe Admin do
    it { should validate_presence_of(:email) }
+   it { should validate_presence_of(:username)}
+   it { should validate_presence_of(:password)}
  end
