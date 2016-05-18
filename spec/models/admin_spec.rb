ae9e3618deb9cc5bf1bb359caf76448f74646c27
jtam11 <justinktam@gmail.com>
2016-05-18 14:18:30 -0700

    Generated spec files for controllers and models

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -5,17 +5,6 @@ RSpec.describe Admin, type: :model do
    subject(:admin) { FactoryGirl.create(:admin) }
 
  end
- #
- # spec/factories/user.rb
- #
- FactoryGirl.define do
-   factory :admin do
-     sequence(:username) { |n| "admin#{n}" }
-     sequence(:email) { |n| "g#{n}@g.com" }
-     password "testtest"
-     confirmed_at { Time.now }
-   end
- end
 
  describe Admin do
    it { should validate_presence_of(:email) }
