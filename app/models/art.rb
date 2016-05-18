7b45fdce924d3d365e95cf34641f700db3b54652
morgansharif <morgan.sharif@gmail.com>
2016-05-17 17:38:40 -0700

    added cascading destroy to art and artist controllers

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,4 +1,11 @@
 class Art < ActiveRecord::Base
   belongs_to :artist
   has_many :questions
+
+  # callback to delete dependant questions first
+  before_destroy :delete_questions
+
+  def delete_questions
+    self.questions.destroy_all
+  end
 end
