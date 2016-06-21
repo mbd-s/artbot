706eb74e7205dbdc9a38135a283880a4735e0c4c
mbd-s <michaelbsilverberg@gmail.com>
2016-06-21 17:49:23 +0200

    added slack button, working on auth tokens

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
-  end  
+    if params['code']
+      rc = HTTP.post("https://slack.com/api/oauth.access", params:
+      client_id: ENV['SLACK_CLIENT_ID'],
+      client_secret: ENV['SLACK_CLIENT_SECRET']
+      code: ENV['SLACK_API_TOKEN'])
+  end
 
 end
